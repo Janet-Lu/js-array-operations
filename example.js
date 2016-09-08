86c051d18981ac3941f992ee59baf53170ea9c27
Janet-Lu <g66519@gmail.com>
2016-09-08 18:02:54 +0800

    Update example.js
    新增中文註解

--- example.js
+++ example.js
@@ -1,79 +1,79 @@
 //Example
 var ary = ["A", "B", "C", "D", "E"];
 
-//length
+//length 取得長度
 console.log(ary.length); //5
 
-//push
+//push 新增一個F在陣列最後方
 console.log(ary.push("F")); //6
 
-//pop
+//pop 刪除陣列最後一個值
 console.log(ary.pop()); //"F"
 
-//unshift
+//unshift 新增一個F在陣列最前方
 console.log(ary.unshift("F")); //6
 
-//shift
+//shift 刪除陣列第一個值
 console.log(ary.shift()); //"F"
 
-//splice(insert)
+//splice(insert) 新增 F 在a[3] 位置
 console.log(ary.splice(3, 0, "F")); //[]
 
-//splice(remove)
+//splice(remove) 刪除a[3]
 console.log(ary.splice(3, 1)); //["F"]
 
-//indexOf
+//indexOf // D在陣列中第一次出現的位置
 console.log(ary.indexOf("D")); //3
 
-//lastIndexOf
+//lastIndexOf D在陣列中最後一次出現的位置
 console.log(ary.lastIndexOf("D")); //3
 
-//join
+//join 用:分開的字串
 console.log(ary.join(":")); //"A:B:C:D:E"
 
-//reverse
+//reverse 陣列反轉
 console.log(ary.reverse()); //["E", "D", "C", "B", "A"]
 
-//sort
+//sort 陣列排序
 console.log(ary.sort()); //["A", "B", "C", "D", "E"]
 
-//some
+//some 看C有沒有在陣列中出現 true=>有,  false=>沒有
 console.log(ary.some(function(item) {
 	return item === "C";
 })); //true
 
-//every
+//every ?????
 console.log(ary.every(function(item) {
 	return item.length === 1;
 })); //true
 
-//forEach
+//forEach  ?????
 console.log(ary.forEach(function(item) {
 	return item;
 })); //undefined
 
-//reduce
+//reduce 陣列轉字串
 console.log(ary.reduce(function(prevItem, item) {
 	return prevItem + item;
 })); //"ABCDE"
 
-//reduceRight
+//reduceRight 反轉陣列轉字串
 console.log(ary.reduceRight(function(prevItem, item) {
 	return prevItem + item;
 })); //"EDCBA"
 
-//map
+//map 陣列...加東西!?
 console.log(ary.map(function(item) {
 	return item + item;
 })); //["AA", "BB", "CC", "DD", "EE"]
 
-//filter
+//filter  依序列出大於C的值
 console.log(ary.filter(function(item) {
 	return item > "C";
 })); //["D", "E"]
 
-//slice
+//slice 取a[2]~a[3]的值
 console.log(ary.slice(2, 4)); //["C", "D"]
 
-//concat
+//concat 陣列最後方加入 ["G", "H"]
 console.log(ary.concat(["G", "H"])); //["A", "B", "C", "D", "E", "G", "H"]
